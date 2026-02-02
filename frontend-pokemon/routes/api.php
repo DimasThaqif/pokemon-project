3700a2efdc4854a6b2687e17b03354fa1c83cb22
dimasthaqif <dimasthaqif10@gmail.com>
2026-02-02 15:21:04 +0700

    fix frontend

--- frontend-pokemon/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PokemonController;
-
-// Route::get('/', [PokemonController::class, 'index']);
-// Route::get('/pokemon/{name}', [PokemonController::class, 'show']);
-
